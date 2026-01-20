dd7068a9612054c0596d3c8bd222de8be4b40d45
AnjanaSruthiR <ranga.anj@northeastern.edu>
2026-01-20 14:09:33 -0500

    Portfolio 2026

---
+++ portfolio/src/data/Experience.js
@@ -0,0 +1,23 @@
+export const experience = [
+  {
+    company: "State Street",
+    role: "REACTJS FULL-STACK DEVELOPER",
+    date: "July 2025 - Dec 2025",
+    logo: "../assets/State-street-logo.png",
+    description: "Spearheaded the end-to-end migration of a legacy .NET and VB application to a modern full-stack system using React.js, Spring Boot, and React MUI, ensuring alignment with organizational design and architecture standards. Enhanced performance and user experience by building responsive UI components, optimizing React rendering, and refining SQL queries, reducing load and execution times by up to 90%."
+  },
+  {
+    company: "Accenture",
+    role: "APPLICATION DEVELOPER",
+    date: "Oct 2022 – July 2024",
+    logo: "../assets/accenture-icon.png",
+    description: "I orchestrated front-end development for 15 cross-platform apps in 17 languages, ensuring seamless multi-device performance. I designed engaging UI/UX using HTML, CSS, and jQuery—integrating social logins and SSO to maintain brand consistency and code efficiency while coordinating cross-functional testing."
+  },
+  {
+    company: "Accenture",
+    role: "SECURITY ENGINEER",
+    date: "Oct 2022 – July 2024",
+    logo: "../assets/accenture-icon.png",
+    description: "I collaborated with the IAM team to securely integrate 10 client apps into Okta via OIDC. I provisioned user access, configured MFA with Okta Verify, and managed user profiles for enhanced security. I implemented a strategy to manage backlog login incidents, reducing resolution time by 60%."
+  }
+];
---
+++ portfolio/src/data/Profile.js
@@ -0,0 +1,12 @@
+export const profile = {
+  name: "Anjana Sruthi",
+  role: "I design secure, scalable, and interactive cutting-edge applications.",
+  status: "Open to Work",
+  bio: "I excel at identifying challenges, streamlining processes, and delivering user-centric solutions that drive efficiency. A fun fact about me is, I love dinosaurs and never miss a rewatch of Jurassic Park!",
+  email: "mailto:anjanasruthi2309@gmail.com",
+  img: "../assets/Profile_Photo.jpg",
+  social: {
+    linkedin: "https://www.linkedin.com/in/anjana-sruthi-ranga-164148211/",
+    github: "https://github.com/AnjanaSruthiR?tab=repositories",
+  }
+};
